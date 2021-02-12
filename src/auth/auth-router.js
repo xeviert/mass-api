7825759fed06fa75e840aeeeee5b05372905408b
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-02-11 18:14:35 -0600

    added user router

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,9 +1,65 @@
 const express = require('express')
 const AuthService = require('./auth-service')
+const { requireAuth } = require('../middleware/jwt-auth')
 
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
 
 authRouter
+    .route('/token')
+    .post(jsonBodyParser, async (req, res, next) => {
+        const { phoneNumber, password } = req.body
+        const loginUser = { phoneNumber, password }
+
+        for (const [key, value] of Object.entries(loginUser))
+            if (value == null)
+                return res.status(400).json({
+                    error: `Missing '${key}' in request body`
+                })
+                
+        try {
+            const dbUser = await AuthService.getUserWithPhoneNumber(
+                req.app.get('db'),
+                loginUser.username
+            )
+
+            if (!dbUser)
+                return res.status(400).json({
+                    error: 'Incorrect username or pw',
+                })
+
+            const compareMatch = await AuthService.comparePasswords(
+                loginUser.password,
+                dbUser.password
+            )
+
+            if (!compareMatch)
+                return res.status(400).json({
+                    error: 'Incorrect username or pw',
+                })
+
+            const sub = dbUser.phoneNumber
+            const payload = {
+                user_id: dbUser.id,
+                phoneNumber: dbUser.phone_number
+            }
+            res.send({
+                authToken: AuthService.createJwt(sub, payload),
+            })
+        } catch (error) {
+            next(error)
+        }
+    })
+
+    .put(requireAuth, (req, res) => {
+        const sub = req.user.phone_number
+        const payload = {
+            user_id: req.user.id,
+            phoneNumber: req.user.phone_number,        
+        }
+        res.send({
+            authToken: AuthService.createJwt(sub, payload)
+        })
+    })
 
 module.exports = authRouter
--- src/user/user-router.js
+++ src/user/user-router.js
@@ -1,6 +1,52 @@
 const express = require('express')
+const path = require('path')
+const UserService = require('./user-service')
 
 const userRouter = express.Router()
 const jsonBodyParser = express.json()
+
+userRouter
+    .post('/', jsonBodyParser, async (req, res, next) => {
+        const { password, phoneNumber, name } = req.body
+
+        for (const field of ['phoneNumber', 'password'])
+            if (!req.body[field])
+                return res.status(400).json({
+                    error: `Missing '${field}' in request body`
+                })
+        try {
+            const passwordError = UserService.validatePassword(password)
+
+            if (passwordError)
+                return res.status(400).json({ error: passwordError })
+
+            const hasUserWithPhoneNumber = await UserService.hasUserWithPhoneNumber(
+                req.app.get('db'),
+                phoneNumber
+            )
+
+            if (hasUserWithPhoneNumber)
+                return res.status(400).json({ error: `Phone number already tied to an existing account` })
+
+            const hashPassword = await UserService.hashPassword(password)
+
+            const newUser = {
+                phone_number,
+                password: hashedPassword,
+            }
+
+            const user = await UserService.insertUser(
+                req.app.get('db'),
+                newUser
+            )
+
+            res
+                .status(201)
+                .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                .json(UserService.serializeUser(user))
+        } catch(error) {
+            next(error)
+        }
+    })
 
 module.exports = userRouter
